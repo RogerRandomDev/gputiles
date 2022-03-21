091aeb86eda019b88d47bbf1a16d83d5ab495183
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2022-03-21 15:03:08 -0700

    Add files via upload

--- gputiles/gpumap.gd
+++ gputiles/gpumap.gd
@@ -6,5 +6,7 @@ func _ready():
 	var img = Image.new();
 	img.create(8,8,false,Image.FORMAT_R8);
 	img.set_pixel(0,0,Color(1,1,1,1))
+	img.set_pixel(0,3,Color(1,1,1,1))
+	img.set_pixel(4,4,Color(1,1,1,1))
 	tex.create_from_image(img)
 	material.set_shader_param("usedTiles",tex)
